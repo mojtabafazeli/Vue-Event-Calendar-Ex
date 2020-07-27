c0eda45a7cfc9a727259a1afe74af927da19ce0c
mojfazel <fazeli.mojtaba@gmail.com>
2020-07-26 22:47:19 -0400

    completed events

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ const $years = ['2020', '2021', '2022', '2023', '2024']
 
 let $today = new Date();
 let $firstDayOfMonth = new Date($today.getUTCFullYear(), $today.getMonth(), 1)
-let $lastDayOfMonth = new Date($today.getUTCFullYear(), $monthsNames.indexOf($today.getMonth()) + 1, 0)
+let $lastDateOfMonth = new Date($today.getUTCFullYear(), $monthsNames.indexOf($today.getMonth()) + 1, 0)
 let $dateObject = {
     day: $today.getDay(),
     date: $today.getDate(),
     month: $monthsNames[$today.getMonth()],
     year: $today.getUTCFullYear(),
     firstDayOfMonth: $firstDayOfMonth.getDay(),
-    lastDayOfMonth: $lastDayOfMonth.getDate()
+    lastDateOfMonth: $lastDateOfMonth.getDate()
 };
 
 
 Vue.component('date', {
-    props: ['i', 'today', 'dateobject', 'event', 'index'],
+    props: ['i', 'today', 'dateobject', 'events', 'eventindex'],
     template: `
      <div class="card date px-2 py-1">
-                        <p :style = "{visibility: i > dateobject.firstDayOfMonth && i <= dateobject.lastDayOfMonth ? 'visible' : 'hidden'}" >
+                        <p :style = "{visibility: i > dateobject.firstDayOfMonth && i <= dateobject.lastDateOfMonth + dateobject.firstDayOfMonth? 'visible' : 'hidden'}" >
                         <span :class = "i - dateobject.firstDayOfMonth == today.date && 
                         dateobject.month == today.month &&
                             dateobject.year == today.year ? 'today-date' : ''">
                             {{i - dateobject.firstDayOfMonth}}
                             </span>
                             </p>
-                        <div class="d-flex justify-content-between">
-                            <span class="mr-1"></span>
-                            <span class="time"></span>
+                        <div div class = "d-flex justify-content-between" v-if="events[eventindex]" >
+                            <span class="mr-1"> {{ events[eventindex].title }}</span>
+                            <span class="time">10:00AM</span>
                         </div>
 
                     </div>
@@ -43,15 +43,18 @@ const app = new Vue({
         months: $monthsNames,
         years: $years,
         dateObject: {},
-        events: [],
+        events: {}
     },
 
     mounted() {
         console.log($dateObject)
-        if (localStorage.getItem('today')) {
-            this.dateObject = JSON.parse(localStorage.getItem('today'));
+        if (localStorage.getItem('date')) {
+            this.dateObject = JSON.parse(localStorage.getItem('date'));
         } else {
             this.dateObject = $dateObject;
+        }
+        if (localStorage.getItem('events')) {
+            this.events = JSON.parse(localStorage.getItem('events'));
         }
     },
 
@@ -63,8 +66,8 @@ const app = new Vue({
             firstDayDate.setFullYear(this.dateObject.year, this.months.indexOf(this.dateObject.month), 1);
             lastDayDate.setFullYear(this.dateObject.year, this.months.indexOf(this.dateObject.month) + 1, 0);
             this.dateObject.firstDayOfMonth = firstDayDate.getDay();
-            this.dateObject.lastDayOfMonth = lastDayDate.getDate();
-            localStorage.setItem('today', JSON.stringify(this.dateObject));
+            this.dateObject.lastDateOfMonth = lastDayDate.getDate();
+            localStorage.setItem('date', JSON.stringify(this.dateObject));
 
         },
         updateYear: function (e) {
@@ -74,17 +77,24 @@ const app = new Vue({
             firstDayDate.setFullYear(this.dateObject.year, this.months.indexOf(this.dateObject.month), 1);
             lastDayDate.setFullYear(this.dateObject.year, this.months.indexOf(this.dateObject.month) + 1, 0);
             this.dateObject.firstDayOfMonth = firstDayDate.getDay();
-            this.dateObject.lastDayOfMonth = lastDayDate.getDate();
-            localStorage.setItem('today', JSON.stringify(this.dateObject));
+            this.dateObject.lastDateOfMonth = lastDayDate.getDate();
+            localStorage.setItem('date', JSON.stringify(this.dateObject));
         },
-        addEvent: function (date, i) {
+        addEvent(date, i) {
             let index = date.year + date.month + (i - date.firstDayOfMonth);
-            this.events[index] = {
+            this.$set(this.events, index, {
                 time: index,
-                title: "title" + index,
-                desc: "desc" + index
-            };
+                title: "title",
+                desc: "desc"
+            });
             console.log(this.events)
+            localStorage.setItem('events', JSON.stringify(this.events));
+        }
+    },
+
+    computed: {
+        updateEvents: function () {
+            return events;
         }
     }
 })
