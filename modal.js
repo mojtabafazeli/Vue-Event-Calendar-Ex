8c54d03265b2f622e83816f20136a28914738916
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-02 10:45:36 -0400

    fixed update issue

--- modal.js
+++ modal.js
@@ -1,12 +1,11 @@
 $('#myModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
-    var title = button.data('title') // Extract info from data-* attributes
+    var index = button.data('index') // Extract info from data-* attributes
     var date = button.data('date') // Extract info from data-* attributes
-    var time = button.data('time') // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
-    modal.find('.title').val(title)
+    modal.find('.title').val(app.getEventTitle(index))
     try {
         modal.find('.date').val(new Date(date).toISOString().substr(0, 10));
     } catch (e) {
@@ -14,7 +13,7 @@ $('#myModal').on('show.bs.modal', function (event) {
     }
 
     try {
-        modal.find('.time').val(time)
+        modal.find('.time').val(app.getEventTime(index))
     } catch (e) {
         modal.find('.time').val('');
     }
